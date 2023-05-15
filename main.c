56cccbfa08210e38653daff2bf42d474cea29a14
escape9179 <tr3.logan@gmail.com>
2023-05-14 23:51:59 -0400

    Replace old console code

--- main.c
+++ main.c
@@ -1,156 +1,151 @@
-/* shoestrings
- * shoestring at bottom of screen
- * shooting falling sneakers.
- */
-
+#include <windows.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
 
-#define SCREEN_WIDTH 121
-#define SCREEN_HEIGHT 25
-#define MAX_BULLETS 10
-#define SLEEP_DUR 5
-#define PLAYER 193
-#define ENEMY 254
-#define BULLET 179
-#define CLEAR_CHAR ' '
-#define SCORE_INC 1
+HANDLE hStdin;
+DWORD fdwSaveOldMode;
 
-typedef char screen[SCREEN_HEIGHT][SCREEN_WIDTH];
-typedef unsigned short ushort;
+VOID ErrorExit(LPCSTR);
+VOID KeyEventProc(KEY_EVENT_RECORD);
+VOID MouseEventProc(MOUSE_EVENT_RECORD);
+VOID ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD);
 
-struct position {
-    ushort x, y;
-} pp, ep, *bparr[MAX_BULLETS];
+int main(VOID)
+{
+    DWORD cNumRead, fdwMode, i;
+    INPUT_RECORD irInBuf[128];
+    int counter=0;
 
-screen scrn;
-short int running = 1;
-static unsigned int fc = 0;
-static int score = 0;
+    // Get the standard input handle.
 
-/**
- * Draw entities and other data on the screen.
- */
-void draw();
+    hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    if (hStdin == INVALID_HANDLE_VALUE)
+        ErrorExit("GetStdHandle");
 
-/**
- * Clear the console of all characters.
- */
-void clr_scrn();
+    // Save the current input mode, to be restored on exit.
 
-/**
- * Update positions of entities and other data.
- */
-void update();
+    if (! GetConsoleMode(hStdin, &fdwSaveOldMode) )
+        ErrorExit("GetConsoleMode");
 
-/**
- * Get a random coordinate along the x-axis.
- */
-ushort rndscrnx();
+    // Enable the window and mouse input events.
 
-int main(void) {
-    pp.x = SCREEN_WIDTH / 2, pp.y = SCREEN_HEIGHT - 1;
-    while (running) {
-        clr_scrn();
-        update();
-        draw();
-        _sleep(SLEEP_DUR);
-    }
-}
+    fdwMode = ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT;
+//    if (! SetConsoleMode(hStdin, fdwMode) )
+//        ErrorExit("SetConsoleMode");
 
-void update() {
-    // Calculate new enemy position
-    // every tick.
-    if (fc % 10 == 0) {
-        ep.y += 1;
-        if (ep.y >= SCREEN_HEIGHT) {
-            ep.y = 0;
-            ep.x = rndscrnx();
-        }
-    }
+    // Loop to read and handle the next 100 input events.
 
-    // Move the bullets, performing any necessary
-    // bound checks.
-    if (fc % 1 == 0) {
-        for (int i = 0; i < MAX_BULLETS; i++) {
+    while (counter++ <= 100)
+    {
+        // Wait for the events.
 
-            // If the bullet pointer is null, do nothing.
-            if (!bparr[i]);
+        if (! ReadConsoleInput(
+                hStdin,      // input buffer handle
+                irInBuf,     // buffer to read into
+                128,         // size of read buffer
+                &cNumRead) ) // number of records read
+            ErrorExit("ReadConsoleInput");
 
-                // If the bullet has reached the top of
-                // the screen, free the memory associated
-                // with the bullet and nullify it.
-            else if ((bparr[i]->y) <= 0) {
-                scrn[bparr[i]->y][bparr[i]->x] = CLEAR_CHAR;
-                free(bparr[i]);
-                bparr[i] = 0;
-            } else {
+        // Dispatch the events to the appropriate handler.
 
-                // Move the bullet up one character
-                // on the y-axis.
-                bparr[i]->y--;
-                ushort bpx = bparr[i]->x;
-                ushort bpy = bparr[i]->y;
+        for (i = 0; i < cNumRead; i++)
+        {
+            switch(irInBuf[i].EventType)
+            {
+                case KEY_EVENT: // keyboard input
+                    KeyEventProc(irInBuf[i].Event.KeyEvent);
+                    break;
 
-                // Check if the bullet position
-                // is equal to the position of the enemy
-                // and delete the enemy if so. Then increment
-                // the score.
-                if (bpx == ep.x && bpy == ep.y) {
-                    ep.x = rndscrnx(), ep.y = 0;
-                    free(bparr[i]), bparr[i] = NULL;
-                    score += SCORE_INC;
-                }
+                case MOUSE_EVENT: // mouse input
+                    MouseEventProc(irInBuf[i].Event.MouseEvent);
+                    break;
+
+                case WINDOW_BUFFER_SIZE_EVENT: // scrn buf. resizing
+                    ResizeEventProc( irInBuf[i].Event.WindowBufferSizeEvent );
+                    break;
+
+                case FOCUS_EVENT:  // disregard focus events
+
+                case MENU_EVENT:   // disregard menu events
+                    break;
+
+                default:
+                    ErrorExit("Unknown event type");
+                    break;
             }
         }
     }
 
-    // Get non-blocking keyboard input.
-    if (_kbhit()) {
-        char c = getch();
-        if (c == 'a')pp.x--;
-        if (c == 'd')pp.x++;
-        if (c == 'q')exit(EXIT_SUCCESS);
-        if (c == 'w') {
+    // Restore input mode on exit.
 
-            // Create another bullet one character
-            // above the player.
-            static int i = 0;
-            bparr[i] = malloc(sizeof(struct position));
-            bparr[i]->x = pp.x, bparr[i]->y = pp.y - 1;
-            if (++i >= MAX_BULLETS - 1)i = 0;
-        }
-    }
+    SetConsoleMode(hStdin, fdwSaveOldMode);
+
+    return 0;
 }
 
-void draw() {
-    printf("Score: %07d\n", score);
-    scrn[pp.y][pp.x] = PLAYER;
-    scrn[ep.y][ep.x] = ENEMY;
-    for (int i = 0; i < MAX_BULLETS; i++) {
-        if (bparr[i])
-            scrn[bparr[i]->y][bparr[i]->x] = BULLET;
-    }
-    for (int i = 0; i < SCREEN_HEIGHT; i++) {
-        puts(scrn[i]);
-    }
-    fc++;
+VOID ErrorExit (LPCSTR lpszMessage)
+{
+    fprintf(stderr, "%s\n", lpszMessage);
+
+    // Restore input mode on exit.
+
+    SetConsoleMode(hStdin, fdwSaveOldMode);
+
+    ExitProcess(0);
 }
 
-void clr_scrn() {
-    system("cls");
-    for (int i = 0; i < SCREEN_HEIGHT; i++) {
-        for (int j = 0; j < SCREEN_WIDTH; j++) {
-            if (j == SCREEN_WIDTH - 1) {
-                scrn[i][j] = '\0';
-                break;
+VOID KeyEventProc(KEY_EVENT_RECORD ker)
+{
+    printf("Key event: ");
+
+    if(ker.bKeyDown)
+        printf("key pressed\n");
+    else printf("key released\n");
+}
+
+VOID MouseEventProc(MOUSE_EVENT_RECORD mer)
+{
+#ifndef MOUSE_HWHEELED
+#define MOUSE_HWHEELED 0x0008
+#endif
+    printf("Mouse event: ");
+
+    switch(mer.dwEventFlags)
+    {
+        case 0:
+
+            if(mer.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
+            {
+                printf("left button press \n");
             }
-            scrn[i][j] = CLEAR_CHAR;
-        }
+            else if(mer.dwButtonState == RIGHTMOST_BUTTON_PRESSED)
+            {
+                printf("right button press \n");
+            }
+            else
+            {
+                printf("button press\n");
+            }
+            break;
+        case DOUBLE_CLICK:
+            printf("double click\n");
+            break;
+        case MOUSE_HWHEELED:
+            printf("horizontal mouse wheel\n");
+            break;
+        case MOUSE_MOVED:
+            printf("mouse moved\n");
+            break;
+        case MOUSE_WHEELED:
+            printf("vertical mouse wheel\n");
+            break;
+        default:
+            printf("unknown\n");
+            break;
     }
 }
 
-ushort rndscrnx() {
-    return (rand() / (float) RAND_MAX) * SCREEN_WIDTH - 1;
+VOID ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD wbsr)
+{
+    printf("Resize event\n");
+    printf("Console screen buffer is %d columns by %d rows.\n", wbsr.dwSize.X, wbsr.dwSize.Y);
 }
