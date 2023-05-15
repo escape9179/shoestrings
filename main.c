400f3c8d4edb6a61182e541521962b3d2a4fefc1
escape9179 <tr3.logan@gmail.com>
2023-05-14 23:55:19 -0400

    Format code

--- main.c
+++ main.c
@@ -5,15 +5,17 @@ HANDLE hStdin;
 DWORD fdwSaveOldMode;
 
 VOID ErrorExit(LPCSTR);
+
 VOID KeyEventProc(KEY_EVENT_RECORD);
+
 VOID MouseEventProc(MOUSE_EVENT_RECORD);
+
 VOID ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD);
 
-int main(VOID)
-{
+int main(VOID) {
     DWORD cNumRead, i;
     INPUT_RECORD irInBuf[128];
-    int counter=0;
+    int counter = 0;
 
     // Get the standard input handle.
 
@@ -23,28 +25,25 @@ int main(VOID)
 
     // Save the current input mode, to be restored on exit.
 
-    if (! GetConsoleMode(hStdin, &fdwSaveOldMode) )
+    if (!GetConsoleMode(hStdin, &fdwSaveOldMode))
         ErrorExit("GetConsoleMode");
 
     // Loop to read and handle the next 100 input events.
 
-    while (counter++ <= 100)
-    {
+    while (counter++ <= 100) {
         // Wait for the events.
 
-        if (! ReadConsoleInput(
+        if (!ReadConsoleInput(
                 hStdin,      // input buffer handle
                 irInBuf,     // buffer to read into
                 128,         // size of read buffer
-                &cNumRead) ) // number of records read
+                &cNumRead)) // number of records read
             ErrorExit("ReadConsoleInput");
 
         // Dispatch the events to the appropriate handler.
 
-        for (i = 0; i < cNumRead; i++)
-        {
-            switch(irInBuf[i].EventType)
-            {
+        for (i = 0; i < cNumRead; i++) {
+            switch (irInBuf[i].EventType) {
                 case KEY_EVENT: // keyboard input
                     KeyEventProc(irInBuf[i].Event.KeyEvent);
                     break;
@@ -54,7 +53,7 @@ int main(VOID)
                     break;
 
                 case WINDOW_BUFFER_SIZE_EVENT: // scrn buf. resizing
-                    ResizeEventProc( irInBuf[i].Event.WindowBufferSizeEvent );
+                    ResizeEventProc(irInBuf[i].Event.WindowBufferSizeEvent);
                     break;
 
                 case FOCUS_EVENT:  // disregard focus events
@@ -76,8 +75,7 @@ int main(VOID)
     return 0;
 }
 
-VOID ErrorExit (LPCSTR lpszMessage)
-{
+VOID ErrorExit(LPCSTR lpszMessage) {
     fprintf(stderr, "%s\n", lpszMessage);
 
     // Restore input mode on exit.
@@ -87,36 +85,28 @@ VOID ErrorExit (LPCSTR lpszMessage)
     ExitProcess(0);
 }
 
-VOID KeyEventProc(KEY_EVENT_RECORD ker)
-{
+VOID KeyEventProc(KEY_EVENT_RECORD ker) {
     printf("Key event: ");
 
-    if(ker.bKeyDown)
+    if (ker.bKeyDown)
         printf("key pressed\n");
     else printf("key released\n");
 }
 
-VOID MouseEventProc(MOUSE_EVENT_RECORD mer)
-{
+VOID MouseEventProc(MOUSE_EVENT_RECORD mer) {
 #ifndef MOUSE_HWHEELED
 #define MOUSE_HWHEELED 0x0008
 #endif
     printf("Mouse event: ");
 
-    switch(mer.dwEventFlags)
-    {
+    switch (mer.dwEventFlags) {
         case 0:
 
-            if(mer.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
-            {
+            if (mer.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED) {
                 printf("left button press \n");
-            }
-            else if(mer.dwButtonState == RIGHTMOST_BUTTON_PRESSED)
-            {
+            } else if (mer.dwButtonState == RIGHTMOST_BUTTON_PRESSED) {
                 printf("right button press \n");
-            }
-            else
-            {
+            } else {
                 printf("button press\n");
             }
             break;
@@ -138,8 +128,7 @@ VOID MouseEventProc(MOUSE_EVENT_RECORD mer)
     }
 }
 
-VOID ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD wbsr)
-{
+VOID ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD wbsr) {
     printf("Resize event\n");
     printf("Console screen buffer is %d columns by %d rows.\n", wbsr.dwSize.X, wbsr.dwSize.Y);
 }
