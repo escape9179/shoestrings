0a8f8a01f846c8e90f4e911af4d96389f8ef4ad0
escape9179 <tr3.logan@gmail.com>
2023-05-19 01:35:55 -0400

    Start rendering screen

--- main.cpp
+++ main.cpp
@@ -7,21 +7,28 @@
 #define CURSOR_LEFT(a) "["#a"D"
 #define CURSOR_RIGHT(a) "["#a"C"
 
+int constexpr SCREEN_WIDTH = 120;
+int constexpr SCREEN_HEIGHT = 30;
 int constexpr READ_BUFFER_SIZE = 32;
 int constexpr UPDATES_PER_SECOND = 1000 / 60;
 int constexpr VK_U = 0x55;
 int constexpr VK_E = 0x45;
 int constexpr VK_O = 0x4F;
+int SCREEN_BUFFER[SCREEN_HEIGHT][SCREEN_WIDTH];
 
 bool enableVirtualTerminalProcessing();
 
 void processKeyEvent(KEY_EVENT_RECORD keyEventRecord);
 
+void renderScreen();
+
 int main() {
-    HANDLE inputHandle = GetStdHandle(STD_INPUT_HANDLE);
     enableVirtualTerminalProcessing();
+    HANDLE inputHandle = GetStdHandle(STD_INPUT_HANDLE);
     while (true) {
+        renderScreen();
         Sleep(UPDATES_PER_SECOND);
+        printf(ESC "[2 q");
         INPUT_RECORD inputRecords[READ_BUFFER_SIZE];
         DWORD numEventsRead;
         PeekConsoleInput(inputHandle, inputRecords, READ_BUFFER_SIZE, &numEventsRead);
@@ -30,11 +37,20 @@ int main() {
         ReadConsoleInput(inputHandle, inputRecords, READ_BUFFER_SIZE, &numEventsRead);
         for (int i = 0; i < numEventsRead; i++) {
             switch (inputRecords[i].EventType) {
-                case KEY_EVENT: {
+                case KEY_EVENT:
                     processKeyEvent(inputRecords[i].Event.KeyEvent);
                     break;
-                }
             }
+        }
+    }
+}
+
+void renderScreen() {
+    for (int y = 0; y < SCREEN_HEIGHT; y++) {
+        for (int x = 0; x < SCREEN_WIDTH; x++) {
+            printf(ESC "[%i;%iH", y, x);
+            if (x % 2 == 0)printf("x");
+            else printf("o");
         }
     }
 }
@@ -51,7 +67,6 @@ bool enableVirtualTerminalProcessing() {
         return false;
     return true;
 }
-
 
 void processKeyEvent(KEY_EVENT_RECORD keyEventRecord) {
     if (!keyEventRecord.bKeyDown)
